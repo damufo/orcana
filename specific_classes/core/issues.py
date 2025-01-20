70b78cf2181f88ec92d7489f92e6fdccc3ff17a6
Dani MF <dani@damufo.eu>
2025-01-20 10:07:53 +0100

    Issues to lenex issues

--- specific_classes/core/issues.py
+++ specific_classes/core/issues.py
@@ -20,17 +20,25 @@ class Issues(list):
     def load_items_from_dbs(self):
         del self[:]  # borra os elementos que haxa
         ISSUES = (
-            (_('DSQ'), 1, _('Disqualified')),  # DES - Descualificación
-            (_('DNF'), 2, _('Did Not Finish')),  # RET - Retiramento
-            (_('DWD'), 3, _('Withdraw')),  # BAI - Baixa
-            (_('DNS'), 4, _('Did Not Start')),  # NPR - Non presentación
-            (_('WSK'), 5, _('Wrong Stroke')),  # DNI - Nado irregular
-            (_('WTR'), 6, _('Wrong Turn')),  # DVI - Viraxe irregular
-            (_('WST'), 7, _('Wrong Start')),  # DSI - Saída irregular
-            (_('WFN'), 8, _('Wrong Finish')),  # DCI - Chegada irregular
-            (_('DFS'), 9, _('False Start')),  # DSA - Saída anticipada
-            (_('DIS'), 10, _('Ilegal start')),  # DPA - Posta anticiapda
-            (_('>15'), 10, _('>15m immersion')),  # >15m inmersión
+            # Issues lenex
+            ('DSQ', 1, _('Athlete/Relay disqualified')),  # Descualificación (xenérica)
+            ('DNS', 2, _('Athlete/Relay did not start \(no reason given or to late withdrawl\)')),  # Non presentación
+            ('DNF', 3, _('Athlete/Relay did not finish')),  # Retiramento (por non poder rematar o comezado)
+            ('WDR', 5, _('Athlete/Relay was withdrawn (on time)')),  # Baixa 
+            ('SICK', 4, _('Athlete is sick.')),  # Baixa por enfermidade
+
+            # Another issues 
+            # ('DSQ', 1, _('Disqualified')),  # DES - Descualificación
+            # ('DNF', 2, _('Did Not Finish')),  # RET - Retiramento
+            # ('DWD', 3, _('Withdraw')),  # BAI - Baixa
+            # ('DNS', 4, _('Did Not Start')),  # NPR - Non presentación
+            # ('WSK', 5, _('Wrong Stroke')),  # DNI - Nado irregular
+            # ('WTR', 6, _('Wrong Turn')),  # DVI - Viraxe irregular
+            # ('WST', 7, _('Wrong Start')),  # DSI - Saída irregular
+            # ('WFN', 8, _('Wrong Finish')),  # DCI - Chegada irregular
+            # ('DFS', 9, _('False Start')),  # DSA - Saída anticipada
+            # ('DIS', 10, _('Ilegal start')),  # DPA - Posta anticiapda
+            # ('>15', 10, _('>15m immersion')),  # >15m inmersión
             )
         (ISSUE_ID, POS, NAME) = range(3) 
         for i in ISSUES:
