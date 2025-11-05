71a67eac32d643ea1e8ab1a32559e7e74be8816e
Dani MF <dani@damufo.eu>
2025-11-05 09:17:02 +0100

    Change gettext generation

--- po/03_reaproveita_traducions.sh
+++ po/03_reaproveita_traducions.sh
@@ -4,6 +4,6 @@
 # gl.po novo a traducir
 
 #msgmerge messages_gl.po gl.po > messages_gl_merged.po
-msgmerge  orcana_version_anterior.po orcana_limpo.po -o orcana_nova_version.po
+msgmerge  orcana.po orcana_limpo.po -o orcana_nova_version.po
 
 exit 0
